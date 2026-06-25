0abe7d247915b6a1ea517e0a6b329df58eeb9d4a
Ann Mariyam Prakash <annmariyamprakash123@gmail.com>
2026-06-25 10:26:43 +0530

    Updated mobile app to use production backend URL

--- chitty_mobile/lib/services/auth_service.dart
+++ chitty_mobile/lib/services/auth_service.dart
@@ -5,13 +5,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   
-  static String get baseUrl {
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8000/api';
-    } else {
-      return 'http://127.0.0.1:8000/api';
-    }
-  }
+  
+   static const String baseUrl =
+      'https://chittyapi.orianacare.com/api';
   
   static Future<List<dynamic>> getCustomers() async {
   final prefs = await SharedPreferences.getInstance();
